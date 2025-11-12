3c9892eb0b2de16fc2a864acedbe206b427add06
Jose <josemanu408@gmail.com>
2025-11-12 10:40:28 +0100

    Ahora hay saltos y gravedad

--- personaje_principal.gd
+++ personaje_principal.gd
@@ -1,6 +1,10 @@
 extends CharacterBody3D
 
 @export var speed : float = 5.0
+@export var fall_acceleration: float = 75.0
+@export var jump_impulse: float = 20
+
+var target_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	
@@ -15,6 +19,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("backward"):
 		direction.z += 1.0	
 		
-	velocity = direction  * speed
+	if is_on_floor() and Input.is_action_pressed("nueva"):
+		target_velocity.y = jump_impulse
+	if not is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+		
+	velocity = target_velocity
 	
 	move_and_slide() 
