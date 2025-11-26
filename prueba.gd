4f1d043a7bbf26936dca9d77800d74b71a822a22
adrian6693 <cuadrosarchillaadrian.fgl@gmail.com>
2025-11-26 10:23:02 +0100

    Menú de pausa sólo con la función de pausar y reanudar

--- prueba.gd
+++ prueba.gd
@@ -7,12 +7,8 @@ var timer2 : SceneTreeTimer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%AnimationPlayer.play("mov_camara")
-	timer1 = get_tree().create_timer(90.5)  
-	await timer1.timeout
-	%AnimationPlayer2.play("mov_camara_2")
-	timer2 = get_tree().create_timer(70.5)  
-	await timer2.timeout
-	%AnimationPlayer3.play("mov_camara_3")
+	%timer1.start()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,9 +17,28 @@ func _process(delta: float) -> void:
 		print("estoy pausando crack")
 		pausa = not pausa
 		get_tree().paused = pausa
+		if %menu_pausa.visible:
+				%menu_pausa.visible = false
+		else:
+			%menu_pausa.visible = true
 
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	print("estoy quitando la pausa crack")
 	pausa = not pausa
 	get_tree().paused = pausa
+
+
+func _on_button_pressed() -> void:
+	pausa = not pausa
+	%menu_pausa.visible = false
+	get_tree().paused = pausa
+
+
+func _on_timer_1_timeout() -> void:
+	%AnimationPlayer2.play("mov_camara_2")
+	%timer2.start()
+
+
+func _on_timer_2_timeout() -> void:
+	%AnimationPlayer3.play("mov_camara_3")
