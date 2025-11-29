14eec05ee9b2cd57d42f250f0991b2f2dadfbc97
Alex <alex.molinaruiz08@gmail.com>
2025-11-29 20:59:18 +0100

    Añadida la interfaz y una interaccion basica proyectil-jugador

--- scripts/personaje_principal.gd
+++ scripts/personaje_principal.gd
@@ -1,12 +1,19 @@
 extends CharacterBody3D
 
-@export var speed : float = 5.0
+signal vida_cambiada(nueva_vida)
+signal he_muerto
+
+@export var speed : float = 100.0
 @export var proyectil:PackedScene
 @export var tiempoEntreDisparo : float = 0.5
+@export var vida : float = 5
 var puedeDisparar : bool = true
-
+var vivo : bool = true
 
 func _physics_process(delta: float) -> void:
+	
+	if not vivo:
+		return
 	
 	var direction = Vector3.ZERO
 	
@@ -15,10 +22,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		direction.x += 1.0
 	if Input.is_action_pressed("left"):
-		direction.x -= 1.0	
+		direction.x -= 1.0
 	if Input.is_action_pressed("backward"):
-		direction.z += 1.0	
-	velocity = direction  * speed
+		direction.z += 1.0
+	velocity = direction  * speed * delta
 	
 	move_and_slide() 
 	
@@ -30,12 +37,22 @@ func disparar():
 	if puedeDisparar:
 		var tmp_proyectil = proyectil.instantiate()
 		tmp_proyectil.position = $Marker3D.global_position
+		if tmp_proyectil.has_method("iniciar"):
+			tmp_proyectil.iniciar(5,5)
 		add_sibling(tmp_proyectil)
 		
 		puedeDisparar = false
 		$timerDisparo.start(tiempoEntreDisparo)
 	
 
-
 func _on_timer_disparo_timeout() -> void:
 	puedeDisparar = true
+
+func recibir_dano(dano: int):
+	print("Ouch")
+	vida = vida - dano
+	vida_cambiada.emit(vida)
+	if vida <= 0 :
+		print("Me morio")
+		vivo = false
+		he_muerto.emit()
--- scripts/proyectil.gd
+++ scripts/proyectil.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
-@export var speed = 1;
+@export var speed = 1
+@export var objetivo = "Enemigos"
+@export var dano = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +15,12 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Enemigos"):
+	if body.is_in_group(objetivo):
 		print("Impacto")
+		if body.has_method("recibir_dano"):
+			body.recibir_dano(dano)
 		queue_free()
+
+func iniciar(nueva_velocidad: float, nuevo_dano: int):
+	speed = nueva_velocidad
+	dano = nuevo_dano
